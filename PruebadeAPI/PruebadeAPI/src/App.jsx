5724550ba9997df1d9d2e317a228a874e191ab1c
sazuniga06 <sazuniga@unicauca.edu.co>
2023-07-26 16:19:39 -0500

    Nuevo fetch

--- PruebadeAPI/PruebadeAPI/src/App.jsx
+++ PruebadeAPI/PruebadeAPI/src/App.jsx
@@ -1,21 +1,50 @@
-import './App.css'
-import { useFetch } from './useFetch'
+import React, { Suspense } from "react";
+//import { useFetch } from "./useFetch";
+import { fetchData } from "./fetchData";
+import "./App.css";
 
-function App() 
-{
-  const {data, loading} = useFetch("https://jsonplaceholder.typicode.com/users")
 
-  return(
+
+// useFetch hook version
+// function App() {
+//   const { data, loading, error, handleCancelRequest } = useFetch(
+//     "https://jsonplaceholder.typicode.com/todos"
+//   );
+
+//   return (
+//     <div className="App">
+//       <h1>Fetch like a Pro</h1>
+
+//       <button onClick={handleCancelRequest}>Cancel Request</button>
+//       <ul className="card">
+//         {error && <li>Error: {error}</li>}
+//         {loading && <li>Loading...</li>}
+//         {data?.map((item) => (
+//           <li key={item.id}>{item.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// --------------------------------------------
+// Pro Version - Render as you fetch
+
+const apiData = fetchData("https://jsonplaceholder.typicode.com/todos");
+
+function App() {
+  const data = apiData.read()
+
+  return (
     <div className="App">
-      <h1> Probando API </h1>
-      <div className = "card">
-        <ul>
-          {loading && <li>Cargando ...</li>}
-          {data?.map((user) => (<li key = {user.id}>{user.name}</li>))}
+      <h1>Fetch like a Pro</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className="card">
+          {data?.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
         </ul>
-      </div>
-    </div> 
-  )
+      </Suspense>
+    </div>
+  );
 }
-
-export default App
+export default App;
