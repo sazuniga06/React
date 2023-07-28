82b6f1e156bf6ca136d7fc9471d380e21a43bef8
sazuniga06 <sazuniga@unicauca.edu.co>
2023-07-28 11:38:31 -0500

    app act

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-
+    return(
+        <h3>Proyecto 3</h3>
+    )
 }
 
 export default App
